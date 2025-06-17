49486094f25ff0741a3698049393a343c718f163
AnaMenoni <anamenonisilva@gmail.com>
2025-06-17 11:36:58 -0300

    funcion vfs-touch terminada

--- src/vfs-touch.c
+++ src/vfs-touch.c
@@ -1,4 +1,4 @@
-#include <stadio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../include/vfs.h"
@@ -7,13 +7,13 @@
 int main (int argc, char *argv[]) {
 	if (argc < 3){
 		fprintf(stderr, "Uso: %s imagen archivo1 [archivo2 ...]\n", argv[0]);
-		return EXIT_FAILURE
+		return EXIT_FAILURE;
 	}
 
 	const char *image_path = argv[1];  // Var con ruta de la imagen
 	
 	for (int i = 2; i < argc; i++){
-		const char *filename = argv[i]
+		const char *filename = argv[i];
 
 		if (!name_is_valid(filename)) {
 			fprintf(stderr, "Error: el nombre '%s' no es válido.\n", filename);
@@ -25,7 +25,7 @@ int main (int argc, char *argv[]) {
 			continue;
 		}
 
-		int inode_number = create_empty_fie_in_free_inode(image_path, 0644); // permisos rw-r--r
+		int inode_number = create_empty_file_in_free_inode(image_path, 0644); // permisos rw-r--r
 		if (inode_number < 0){
 			fprintf(stderr, "Error: no se pudo crear el archivo '%s' (no hay inodos libres).\n", filename);
 			continue;
